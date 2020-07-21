137cf16ef42af8fcc676a273cf3681fe86c38d96
Sharon Xia <sx73@drexel.edu>
2020-07-21 00:45:10 -0400

    added streaks

--- mood.py
+++ mood.py
@@ -1,11 +1,23 @@
 #!flask/bin/python
 from flask import Flask, jsonify, request
+import datetime
 
 app = Flask(__name__)
 users = {
 	"John123": {
 		"password": "123",
-		"mood": ["happy"]
+		"mood": ["happy", "sad"],
+		"startDate": datetime.datetime(2020, 7, 19).strftime('%m/%d/%Y'),
+		"lastUpdatedDate": datetime.datetime(2020, 7, 20).strftime('%m/%d/%Y'),
+		"streak": 2
+	},
+
+	"Sally456": {
+		"password": "456",
+		"mood": ["hungry", "sad", "tired"],
+		"startDate": datetime.datetime(2020, 7, 16).strftime('%m/%d/%Y'),
+		"lastUpdatedDate": datetime.datetime(2020, 7, 18).strftime('%m/%d/%Y'),
+		"streak": 3
 	}
 }
 
@@ -14,25 +26,41 @@ def index():
 	if (request.method == 'POST'):
 		if request.json["username"] in users:
 			if request.json["password"] == users[request.json["username"]]["password"]:
+				if (datetime.date.today() - datetime.datetime.strptime(users[request.json["username"]]["lastUpdatedDate"], '%m/%d/%Y').date()).days > 1:
+					users[request.json["username"]]["startDate"] = datetime.date.today().strftime('%m/%d/%Y')
+					users[request.json["username"]]["lastUpdatedDate"] = datetime.date.today().strftime('%m/%d/%Y')
+					users[request.json["username"]]["streak"] = 1
+				else:
+					users[request.json["username"]]["lastUpdatedDate"] = datetime.date.today().strftime('%m/%d/%Y')
+					users[request.json["username"]]["streak"] += 1
+
 				users[request.json["username"]]["mood"].append(request.json["newMood"])
 				return jsonify({"Updated mood": request.json["newMood"]})
-		
+			else:
+				return jsonify({"Error": "Incorrect password."})
+
 		else :
 			users[request.json["username"]] = {
 			"password": request.json["password"],
-			"mood": [request.json["newMood"]]
+			"mood": [request.json["newMood"]],
+			"startDate": datetime.date.today().strftime('%m/%d/%Y'),
+			"lastUpdatedDate": datetime.date.today().strftime('%m/%d/%Y'),
+			"streak": 1
 			}
+
+			print(users)
 
 			return jsonify({'Created user': request.json["username"]})
 	else:
 		if request.json["username"] in users:
 			if request.json["password"] == users[request.json["username"]]["password"]:
-				return jsonify({"All your moods": users[request.json["username"]]["mood"]})
+				return jsonify({"All your moods": users[request.json["username"]]["mood"], 
+					"Current mood": users[request.json["username"]]["mood"][-1],
+					"Current streak": users[request.json["username"]]["streak"]})
 			else:
 				return jsonify({"Error": "Incorrect password."})
 		else:
 			return jsonify({"Error": "Please make an account."})
-		return jsonify({"Current user": users})
 
 if __name__ == '__main__':
     app.run(debug=True)
