26c00da67e671058c6f41270eb47ab499a3d0c45
Sharon Xia <sx73@drexel.edu>
2020-07-20 22:55:05 -0400

    allow user login

--- mood.py
+++ mood.py
@@ -2,19 +2,37 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-moods = []
+users = {
+	"John123": {
+		"password": "123",
+		"mood": ["happy"]
+	}
+}
 
 @app.route('/mood', methods = ['GET', 'POST'])
 def index():
-
 	if (request.method == 'POST'):
-		moodValue = request.json["newMood"]
-		moods.append(moodValue)
-		return jsonify({'Set mood to': moodValue})
+		if request.json["username"] in users:
+			if request.json["password"] == users[request.json["username"]]["password"]:
+				users[request.json["username"]]["mood"].append(request.json["newMood"])
+				return jsonify({"Updated mood": request.json["newMood"]})
+		
+		else :
+			users[request.json["username"]] = {
+			"password": request.json["password"],
+			"mood": [request.json["newMood"]]
+			}
+
+			return jsonify({'Created user': request.json["username"]})
 	else:
-		if len(moods) == 0:
-			return "No moods, add one."
-		return jsonify({"Current mood": moods[-1]})
+		if request.json["username"] in users:
+			if request.json["password"] == users[request.json["username"]]["password"]:
+				return jsonify({"All your moods": users[request.json["username"]]["mood"]})
+			else:
+				return jsonify({"Error": "Incorrect password."})
+		else:
+			return jsonify({"Error": "Please make an account."})
+		return jsonify({"Current user": users})
 
 if __name__ == '__main__':
     app.run(debug=True)
